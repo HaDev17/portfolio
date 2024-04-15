1e86a3bf0f9343fc7a43a887d926ce221c44b9e4
HaDev17 <104528704+HaDev17@users.noreply.github.com>
2024-04-15 18:02:56 +0100

    hbb

--- src/components/teaching/Courses.js
+++ src/components/teaching/Courses.js
@@ -5,8 +5,8 @@ function Courses(props) {
         <div style={{ display: 'grid', gridTemplateColumns: '0fr .2fr .5fr .5fr ', gap: '10px' , overflowY: 'auto'}}>
           <div className='pe-2 text-white'>University</div>
           <div className='text-white'>Level</div>
-          <div className='text-white'>Course</div>
           <div className='text-white'>Speciality</div>
+          <div className='text-white'>Course</div>
           {props.courses.map((course, index) => {
             const details = course.split(', ');
             return details.map((detail, i) => <div key={`${index}-${i}`}>{detail}</div>);
